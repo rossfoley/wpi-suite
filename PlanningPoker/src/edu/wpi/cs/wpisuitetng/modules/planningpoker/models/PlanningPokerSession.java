3624ce0f3818e499c47d82bd23f7554c5ef0d3ca
Ross Foley <ross3141@gmail.com>
2014-03-24 21:12:35 -0400

    Added basic PlanningPokerSession model with name and endDate

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/PlanningPokerSession.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+
+/**
+ * @author rossfoley
+ *
+ */
+public class PlanningPokerSession extends AbstractModel {
+	
+	private String name;
+	private GregorianCalendar endDate;
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#save()
+	 */
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#delete()
+	 */
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#toJSON()
+	 */
+	@Override
+	public String toJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wpi.cs.wpisuitetng.modules.Model#identify(java.lang.Object)
+	 */
+	@Override
+	public Boolean identify(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
