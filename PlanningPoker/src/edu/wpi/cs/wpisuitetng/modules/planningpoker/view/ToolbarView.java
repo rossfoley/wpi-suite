64910f6414010485a84aec056ed372465d417b3e
Jonathan Dorich <jdorich@wpi.edu>
2014-03-25 20:43:41 -0400

    Copied Requirement Manager view/gui files into planning poker view package

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/ToolbarView.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/ToolbarView.java
@@ -1,29 +1,74 @@
-package edu.wpi.cs.wpisuitetng.modules.planningpoker.view;
+/*******************************************************************************
+ * Copyright (c) 2013 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Team Rolling Thunder
+ ******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view;
 
-import javax.swing.JToolBar;
+
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.DefaultToolbarView;
+import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.buttons.ChartButtonsPanel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.buttons.EditButtonsPanel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.buttons.RequirementButtonsPanel;
 
 /**
- * This is the toolbar for the PlanningPoker module
+ * Sets up upper toolbar of RequirementManager tab
  * 
- * @author jdorich
- * 
+ *
+ * @version $Revision: 1.0 $
+ * @author justinhess
  */
-@SuppressWarnings("serial")
-public class ToolbarView extends JToolBar {
+public class ToolbarView  extends DefaultToolbarView {
 
-    /** The panel containing toolbar buttons */
-    private final ToolbarPanel toolbarPanel;
+	public ChartButtonsPanel chartButton = new ChartButtonsPanel();
+	public EditButtonsPanel editButton = new EditButtonsPanel();
+	public RequirementButtonsPanel reqButton = new RequirementButtonsPanel();
+	
+	/**
+	 * Creates and positions option buttons in upper toolbar
+	 * @param visible boolean
+	 */
+	public ToolbarView(boolean visible) {
 
-    /**
-     * Construct this view and all components in it.
-     */
-    public ToolbarView() {
+		this.addGroup(reqButton);
+		this.addGroup(chartButton);
+		this.addGroup(editButton);
 
-        // Prevent this toolbar from being moved
-        setFloatable(false);
+	}
+	
+	/**
+	 * Method getEditButton.
+	
+	 * @return EditButtonsPanel */
+	public EditButtonsPanel getEditButton(){
+		return editButton;
+	}
+	
+	/**
+	 * Method getChartButton.
+	
+	 * @return ChartButtonsPanel */
+	public ChartButtonsPanel getChartButton() {
+		return chartButton;
+	}
 
-        // Add the panel containing the toolbar buttons
-        toolbarPanel = new ToolbarPanel();
-        add(toolbarPanel);
-    }
+	/**
+	 * Method getReqButton.
+	
+	 * @return RequirementButtonsPanel */
+	public RequirementButtonsPanel getReqButton() {
+		return reqButton;
+	}
 }
