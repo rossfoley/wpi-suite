156cab78832ea0e7181570fcb166eaffddec783e
Brian <bmflynn@wpi.edu>
2014-03-28 15:26:43 -0400

    associated PlannerPoker with RequirementManager project for adding requirements with Kevin and Andrew

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/PlanningPokerSession.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/PlanningPokerSession.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import com.google.gson.Gson;
 
 import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
 /**
  * @author rossfoley
@@ -20,6 +21,10 @@ public class PlanningPokerSession extends AbstractModel {
 	private GregorianCalendar endDate;
 	private Set<Integer> requirementIDs;
 	private int ID;
+	/**
+	 * The list in which all the requirements for a single project are contained
+	 */
+	private List<Requirement> requirements;
 	
 
 	/**
@@ -125,5 +130,36 @@ public class PlanningPokerSession extends AbstractModel {
 		final Gson parser = new Gson();
 		return parser.fromJson(json, PlanningPokerSession.class);
 	}
+	
+	/**
+	 * Returns the Requirement with the given ID
+	 * 
+	 * @param id The ID number of the requirement to be returned
+	
+	 * @return the requirement for the id or null if the requirement is not found */
+	public Requirement getRequirement(int id)
+	{
+		Requirement temp = null;
+		// iterate through list of requirements until id is found
+		for (int i=0; i < this.requirements.size(); i++){
+			temp = requirements.get(i);
+			if (temp.getId() == id){
+				break;
+			}
+		}
+		return temp;
+	}
+	
+	/**
+	 * Provides the number of elements in the list of requirements for the project. This
+	 * function is called internally by the JList in NewRequirementPanel. Returns elements
+	 * in reverse order, so the newest requirement is returned first.
+	 * 	
+	 * @return the number of requirements in the project * @see javax.swing.ListModel#getSize() * @see javax.swing.ListModel#getSize() * @see javax.swing.ListModel#getSize()
+	 */
+	public int requirementsGetSize() {
+		return requirements.size();
+	}
+
 
 }
