dffe7362492a0da45e9362a614a6e29e8c43566c
Rick Wight <fmwight@wpi.edu>
2014-03-31 21:23:20 -0400

    added more comprehensive testign of functions working with requirementIDs

--- PlanningPoker/test/tests/PlanningPokerTests.java
+++ PlanningPoker/test/tests/PlanningPokerTests.java
@@ -6,11 +6,15 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.PlanningPokerSession;
 
 /**
+ * Tests of the PlanningPoker
  * 
  * @author Rick Wight (fmwight) 
  *
@@ -19,9 +23,14 @@ public class PlanningPokerTests {
 	
 	PlanningPokerSession testSession = new PlanningPokerSession(); 
 
+	/**
+	 * test to see that the test session is created and that it's fields were modified correctly
+	 */
 	@Test
 	public void testIfSessionExists() {
 		assertNotNull(testSession); 
+		assertFalse(testSession.isOpen()); 
+		assertNotNull(testSession.getName()); 
 	}
 	
 	@Test 
@@ -32,13 +41,45 @@ public class PlanningPokerTests {
 	
 	@Test 
 	public void testGetSetName() {
-		testSession.setName("The name is Test Session"); 
-		assertEquals("The name is Test Session", testSession.getName()); 
+		testSession.setName("The name of a Test Session"); 
+		assertEquals("The name of a Test Session", testSession.getName()); 
+	}
+	
+	/**
+	 * test if the get/set/add requirementID functions work correctly 
+	 */
+	@Test
+	public void testReqIDs() {
+		/* 
+		 * right now this is flawed, in that setRequirementIDs MUST be called before
+		 * addRequirement, because the set of requirements is not initialized in the 
+		 * constructor. This will be revisited after updated code is merged in. 
+		 */
+		HashSet<Integer> aSet = new HashSet<Integer>(); 
+		testSession.setRequirementIDs(aSet); 
+		assertNotNull(testSession.getRequirementIDs()); 
+		testSession.addRequirement(5); 
+		testSession.addRequirement(4); 
+		testSession.addRequirement(5); 
+		assertEquals(2, testSession.getRequirementIDs().size()); 
+		assertTrue(testSession.getRequirementIDs().contains(4)); 
+		assertTrue(testSession.getRequirementIDs().contains(5)); 
+	}
+	
+	/**
+	 * test get/add requirement functions
+	 * NOTE: these are currently incomplete. there is a method to get a requirement by
+	 * it's ID number, but not to set the requirement in the first place. test will be
+	 * written when that function exists, for now it has a placeholder
+	 */
+	@Test
+	public void testReqs() {
+		assertTrue(true); 
 	}
 	
 	/** 
 	 * test if the toJSON() and fromJson() methods preserve the session information 
-	 * by checking that the ID and name fields retain the same values  
+	 * by checking that the ID and name fields retain the same values 
 	 */
 	@Test 
 	public void testToAndFromJSONSessionNameID() {
