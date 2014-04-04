f6dc239fab2cab5f18cfbb689a83e36fd1214f32
Jonathan Dorich <jdorich@wpi.edu>
2014-04-03 21:27:22 -0400

    Refactored BetterNewGameTab to align better with the ViewEventController methods and renamed to PlanningPokerSessionTab

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/gui/ViewMode.java
@@ -0,0 +1,11 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.gui;
+
+/**
+ * Used to check if a planning poker session is in editing mode or being created
+ *
+ */
+public enum ViewMode 
+{
+	EDITING,
+	CREATING;
+}
