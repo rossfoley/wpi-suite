4f98b46446b86e43f267a5061b453864b62c5aae
Amanda Adkins <aaadkins@wpi.edu>
2014-04-05 16:52:12 -0400

    Add methods to AddDeckRequestObserver

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddDeckRequestObserver.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/AddDeckRequestObserver.java
@@ -20,7 +20,7 @@ public class AddDeckRequestObserver {
 	}
 	
 	/**
-	 * Parse the session that was received from the server then pass them to
+	 * Parse the deck that was received from the server then pass them to
 	 * the controller.
 	 * 
 	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseSuccess(edu.wpi.cs.wpisuitetng.network.models.IRequest)
@@ -35,6 +35,28 @@ public class AddDeckRequestObserver {
 		final Deck addedDeck = Deck.fromJson(response.getBody());		
 	}
 	
+	/**
+	 * Takes an action if the response results in an error.
+	 * Specifically, outputs that the request failed.
+	 * @param iReq IRequest
+	
+	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#responseError(IRequest) */
+	//@Override
+	public void responseError(IRequest iReq) {
+		System.err.println("The request to add a deck failed. Response Error");
+	}
+	
+	/**
+	 * Takes an action if the response fails.
+	 * Specifically, outputs that the request failed.
+	 * @param iReq IRequest
+	 * @param exception Exception
+	
+	 * @see edu.wpi.cs.wpisuitetng.network.RequestObserver#fail(IRequest, Exception) */
+	//@Override
+	public void fail(IRequest iReq, Exception exception) {
+		System.err.println("The request to add a deck failed. FAIL!");
+	}
 	
 	
 	
