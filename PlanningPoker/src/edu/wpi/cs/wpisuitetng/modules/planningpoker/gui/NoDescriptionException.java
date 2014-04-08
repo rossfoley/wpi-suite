238f3d50f50638fbe8a3b9ce6abd1a84249c3eb3
Pat Desmarais <pdesmarais@wpi.edu>
2014-04-08 18:31:11 -0400

    Added NoDescriptionException class

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/gui/NoDescriptionException.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.gui;
+
+/**
+ * @author Pat Desmarais & Cameron Peterson
+ *
+ */
+public class NoDescriptionException extends Exception{
+	public NoDescriptionException(){}
+
+}
