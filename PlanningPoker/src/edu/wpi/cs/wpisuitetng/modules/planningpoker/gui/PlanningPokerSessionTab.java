4d5553acb61bc3ce19a6adad8d6dd020a81a8d01
Ross Foley <ross3141@gmail.com>
2014-04-07 21:53:04 -0400

    Remove old month dropdown code

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/gui/PlanningPokerSessionTab.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/gui/PlanningPokerSessionTab.java
@@ -17,15 +17,26 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.gui;
  *
  */
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
-import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
+import javax.swing.SpringLayout;
 
+import net.sourceforge.jdatepicker.JDateComponentFactory;
+import net.sourceforge.jdatepicker.JDatePicker;
+import net.sourceforge.jdatepicker.impl.UtilCalendarModel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.DeckListModel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.PlanningPokerSession;
@@ -58,9 +69,6 @@ public class PlanningPokerSessionTab extends JPanel {
 	JPanel secondPanel = new JPanel();
 	
 	private ViewMode viewMode;
-	JComboBox<Months> comboMonth = new JComboBox<Months>();
-	JComboBox<String> comboDay = new JComboBox<String>();
-	JComboBox<String> comboYear = new JComboBox<String>();
 	JComboBox<String> comboTime = new JComboBox<String>();
 	JComboBox<String> comboAMPM = new JComboBox<String>();
 	JComboBox<String> comboDeck = new JComboBox<String>();
@@ -88,7 +96,6 @@ public class PlanningPokerSessionTab extends JPanel {
 		this.buildLayouts();
 		this.displayPanel(firstPanel);
 	}
-	
 	
 	/**
 	 * Constructor for editing an existing planning poker session
@@ -132,14 +139,10 @@ public class PlanningPokerSessionTab extends JPanel {
 		textFieldSessionField.setText(defaultName);
 		textFieldSessionField.setColumns(10);
 		textFieldDescription.setColumns(10);
-		comboMonth.setBackground(Color.WHITE);
-		comboDay.setBackground(Color.WHITE);	
-		comboYear.setBackground(Color.WHITE);
 		comboTime.setBackground(Color.WHITE);
 		comboAMPM.setBackground(Color.WHITE);
 		comboDeck.setBackground(Color.WHITE);
 		lblSessionEndTime.setForeground(Color.BLACK);
-		comboMonth.setModel(new DefaultComboBoxModel<Months>(Months.values()));
 		comboAMPM.setModel(new DefaultComboBoxModel<String>(new String[] {"AM","PM"}));
 		descriptionErrorMessage.setForeground(Color.RED);
 		nameErrorMessage.setForeground(Color.RED);
@@ -154,10 +157,9 @@ public class PlanningPokerSessionTab extends JPanel {
 		firstPanelLayout.putConstraint(SpringLayout.SOUTH, btnNext, -10, SpringLayout.SOUTH, firstPanel);
 		firstPanelLayout.putConstraint(SpringLayout.EAST, btnNext, -10, SpringLayout.EAST, firstPanel);
 		
-		firstPanelLayout.putConstraint(SpringLayout.SOUTH, (JPanel) datePicker, -10, SpringLayout.SOUTH, firstPanel);
-		firstPanelLayout.putConstraint(SpringLayout.EAST, (JPanel) datePicker, 10, SpringLayout.WEST, firstPanel);
-
-		firstPanelLayout.putConstraint(SpringLayout.EAST, comboDay, 70, SpringLayout.EAST, comboMonth);							
+		firstPanelLayout.putConstraint(SpringLayout.EAST, (JPanel) datePicker, 10, SpringLayout.EAST, lblEndDate);
+		firstPanelLayout.putConstraint(SpringLayout.NORTH, (JPanel) datePicker, 6, SpringLayout.SOUTH, lblEndDate);
+		firstPanelLayout.putConstraint(SpringLayout.WEST, (JPanel) datePicker, 0, SpringLayout.WEST, lblEndDate);					
 		
 		firstPanelLayout.putConstraint(SpringLayout.NORTH, lblSessionName, 10, SpringLayout.NORTH, firstPanel);
 		firstPanelLayout.putConstraint(SpringLayout.WEST, lblSessionName, 10, SpringLayout.WEST, firstPanel);		
@@ -174,21 +176,9 @@ public class PlanningPokerSessionTab extends JPanel {
 		firstPanelLayout.putConstraint(SpringLayout.SOUTH, textFieldDescription, -175, SpringLayout.SOUTH, firstPanel);
 		firstPanelLayout.putConstraint(SpringLayout.EAST, textFieldDescription, -10, SpringLayout.EAST, firstPanel);					
 
-		firstPanelLayout.putConstraint(SpringLayout.EAST, comboMonth, 10, SpringLayout.EAST, lblEndDate);
 		firstPanelLayout.putConstraint(SpringLayout.NORTH, lblEndDate, 6, SpringLayout.SOUTH, textFieldDescription);
-		firstPanelLayout.putConstraint(SpringLayout.WEST, lblEndDate, 0, SpringLayout.WEST, lblSessionName);
-		
-		firstPanelLayout.putConstraint(SpringLayout.NORTH, comboMonth, 6, SpringLayout.SOUTH, lblEndDate);
-		firstPanelLayout.putConstraint(SpringLayout.WEST, comboMonth, 0, SpringLayout.WEST, lblEndDate);
-								
-		firstPanelLayout.putConstraint(SpringLayout.NORTH, comboDay, 6, SpringLayout.SOUTH, lblEndDate);
-		firstPanelLayout.putConstraint(SpringLayout.WEST, comboDay, 6, SpringLayout.EAST, comboMonth);			
-		
-		firstPanelLayout.putConstraint(SpringLayout.WEST, comboYear, 6, SpringLayout.EAST, comboDay);
-		firstPanelLayout.putConstraint(SpringLayout.EAST, comboYear, 90, SpringLayout.EAST, comboDay);
-		firstPanelLayout.putConstraint(SpringLayout.NORTH, comboYear, 0, SpringLayout.NORTH, comboMonth);		
-		
-		firstPanelLayout.putConstraint(SpringLayout.NORTH, lblSessionEndTime, 6, SpringLayout.SOUTH, comboMonth);
+		firstPanelLayout.putConstraint(SpringLayout.WEST, lblEndDate, 0, SpringLayout.WEST, lblSessionName);		
+
 		firstPanelLayout.putConstraint(SpringLayout.WEST, lblSessionEndTime, 0, SpringLayout.WEST, lblSessionName);
 		
 		firstPanelLayout.putConstraint(SpringLayout.NORTH, comboTime, 6, SpringLayout.SOUTH, lblSessionEndTime);
@@ -219,43 +209,10 @@ public class PlanningPokerSessionTab extends JPanel {
 		firstPanelLayout.putConstraint(SpringLayout.WEST, dateErrorMessage, 20, SpringLayout.EAST, lblEndDate);
 		
 		// Handle the time dropdowns
-		setDays31();
 		setTimeDropdown();
-		setYearDropdown();
 		parseTimeDropdowns();
 		setDeckDropdown();
-		
-		// Month dropdown event handler
-		comboMonth.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				handleMonthChange();
-			}	
-		});
-		
-		// Day dropdown event handler
-		comboDay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				 parseDayDropdown();
-			}	
-		});
-		
-		// Year dropdown event handler
-		comboYear.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e){
-				String yearString = (String) comboYear.getSelectedItem();
-				if (yearString.equals("Year")) {
-					year = 1;
-				}
-				else {
-					year = Integer.parseInt(yearString);
-				}
-				if (month == 1) {
-					setFebDays();
-				}
-				parseDayDropdown();
-			}	
-		});
-		
+
 		// Time dropdown event handler
 		comboTime.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
@@ -333,9 +290,9 @@ public class PlanningPokerSessionTab extends JPanel {
 		firstPanel.add(lblSessionDescription);
 		firstPanel.add(textFieldDescription);
 		firstPanel.add(lblEndDate);
-		firstPanel.add(comboMonth);
+		/*firstPanel.add(comboMonth);
 		firstPanel.add(comboDay);
-		firstPanel.add(comboYear);
+		firstPanel.add(comboYear);*/
 		firstPanel.add(comboTime);
 		firstPanel.add(comboAMPM);
 		firstPanel.add(comboDeck);
@@ -421,125 +378,6 @@ public class PlanningPokerSessionTab extends JPanel {
 		newPanel.repaint();
 	}
 	
-	private void handleMonthChange() {
-		Months monthEnum = (Months) comboMonth.getSelectedItem();
-		switch(monthEnum){
-		case Month:
-			month = 13;
-			setDays31();
-			break;
-		case JANUARY:
-			month = 0;
-			setDays31();
-			break;
-		case FEBRUARY:
-			month = 1;
-			setFebDays();
-			break;
-		case MARCH:
-			month = 2;
-			setDays31();
-			break;
-		case APRIL:
-			month = 3;
-			setDays30();
-			break;
-		case MAY:
-			month = 4;
-			setDays31();
-			break;
-		case JUNE:
-			month = 5;
-			setDays30();
-			break;
-		case JULY:
-			month = 6;
-			setDays31();
-			break;
-		case AUGUST:
-			month = 7;
-			setDays31();
-			break;
-		case SEPTEMBER:
-			month = 8;
-			setDays30();
-			break;
-		case OCTOBER:
-			month = 9;
-			setDays31();
-			break;
-		case NOVEMBER:
-			month = 10;
-			setDays30();
-			break;
-		case DECEMBER:
-			month = 11;
-			setDays31();
-			break;
-		default:
-			month = 0;
-			setDays31();
-			break;
-		}
-		parseDayDropdown();
-	}
-	
-	/** 
-	 * chanes the day dropdown to have 31 days
-	 */
-	public void setDays31(){
-		if (displayingDays!=31){
-			comboDay.setModel(new DefaultComboBoxModel<String>(new String[] {"Day", "1", "2", "3", "4","5", "6", "7", "8",
-					"9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19", "20",
-					"21", "22","23", "24", "25", "26", "27", "28", "29", "30", "31" }));
-			displayingDays = 31;
-		}
-	}
-	
-	/**
-	 * changes the day dropdown to have 30 days
-	 */
-	public void setDays30(){
-		if (displayingDays!=30){
-			comboDay.setModel(new DefaultComboBoxModel<String>(new String[] {"Day", "1", "2", "3", "4","5", "6", "7", "8",
-					"9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19", "20",
-					"21", "22","23", "24", "25", "26", "27", "28", "29", "30" }));
-			displayingDays = 30;
-		}
-	}
-
-	/**
-	 * repopulate the day dropdown menu if February is selected 
-	 */
-	public void setFebDays(){
-		if ((year%4)==0){
-			if (displayingDays!=29){
-				comboDay.setModel(new DefaultComboBoxModel<String>(new String[] {"Day", "1", "2", "3", "4","5", "6", "7", "8",
-						"9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19", "20",
-						"21", "22","23", "24", "25", "26", "27", "28", "29"}));
-				displayingDays = 29;
-			}
-		}
-		else {
-			if (displayingDays!=28){
-				comboDay.setModel(new DefaultComboBoxModel<String>(new String[] {"Day", "1", "2", "3", "4","5", "6", "7", "8",
-						"9", "10", "11", "12","13", "14", "15", "16", "17", "18", "19", "20",
-						"21", "22","23", "24", "25", "26", "27", "28"}));
-				displayingDays = 28;
-			}
-		}
-	}
-	
-	/**
-	 * populates the drop down menu for the year
-	 */
-	public void setYearDropdown(){
-		comboYear.setModel(new DefaultComboBoxModel<String>(new String[] {
-				"Year", "2014", "2015", "2016", "2017", "2018", "2019" , "2020", 
-				"2021", "2022", "2023", "2024", "2025", "2026", "2027", "2028", "2029", "2030", 
-				"2031", "2032", "2033", "2034"}));
-	}
-	
 	/**
 	 * populates the drop down menu for the time
 	 */
@@ -561,9 +399,6 @@ public class PlanningPokerSessionTab extends JPanel {
 		}
 		comboDeck.setModel(new DefaultComboBoxModel<String>(deckNames));
 	}
-	
-
-	
 	
 	/**
 	 * takes the strings from the time dropdown menus, parses the strings, and saves the hour and minute to the appopriate fields
@@ -621,10 +456,8 @@ public class PlanningPokerSessionTab extends JPanel {
 	 * and removes this tab.
 	 */
 	private void submitSessionToDatabase() {
-		
 		PlanningPokerSessionModel.getInstance().addPlanningPokerSession(pokerSession);		
 		ViewEventController.getInstance().removeTab(this);// this thing closes the tabs
-		
 	}
 
 	/**
@@ -633,18 +466,4 @@ public class PlanningPokerSessionTab extends JPanel {
 	public PlanningPokerSession getDisplaySession() {
 		return pokerSession;
 	}
-
-	/**
-	 * parse the day dropdown menu and set the day field appropriately
-	 */
-	public void parseDayDropdown(){
-		String dayString = (String) comboDay.getSelectedItem();
-		if (dayString.equals("Day")){
-			day = 0;
-		}
-		else {
-			day = Integer.parseInt(dayString);
-		}
-	}
-
 }
