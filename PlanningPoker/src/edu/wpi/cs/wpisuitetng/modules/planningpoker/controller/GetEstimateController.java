cd9079c68e237f0ae90fd8b5803f6c17a84a69ee
Kevin <kjb594@yahoo.com>
2014-04-13 19:40:07 -0400

    Adds the controllers for getting, updating and adding an Estimate. An Estimate is a code which has the name of the owner, the vote, the ID of the requirement, the ID of the estimate, and the ID of the owner. The managerLayer also now has a path in there for planningpoker/estimate.

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/controller/GetEstimateController.java
@@ -0,0 +1,80 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Estimate;
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.EstimateModel;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
+
+public class GetEstimateController implements ActionListener {
+
+	private GetEstimateRequestObserver observer;
+	private static GetEstimateController instance;
+
+	/**
+	 * Constructs the controller given a RequirementModel
+	 */
+	private GetEstimateController(){
+		
+		observer = new GetEstimateRequestObserver(this);
+	}
+	
+	/**
+	
+	 * @return the instance of the GetRequirementController or creates one if it does not
+	 * exist. */
+	public static GetEstimateController getInstance()
+	{
+		if(instance == null)
+		{
+			instance = new GetEstimateController();
+		}
+		
+		return instance;
+	}
+
+	/**
+	 * Sends an HTTP request to store a requirement when the
+	 * update button is pressed
+	 * @param e ActionEvent
+	
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent) */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// Send a request to the core to save this estimate
+		final Request request = Network.getInstance().makeRequest("planningpoker/estimate", HttpMethod.GET); // GET == read
+		request.addObserver(observer); // add an observer to process the response
+		request.send(); // send the request
+	}
+	
+	/**
+	 * Sends an HTTP request to retrieve all requirements
+	 */
+	public void retrieveEstimates() {
+		final Request request = Network.getInstance().makeRequest("planningpoker/estimate", HttpMethod.GET); // GET == read
+		request.addObserver(observer); // add an observer to process the response
+		request.send(); // send the request
+	}
+
+	/**
+	 * Add the given requirements to the local model (they were received from the core).
+	 * This method is called by the GetRequirementsRequestObserver
+	 * 
+	 * @param requirements array of requirements received from the server
+	 */
+	
+	public void receivedEstimates(Estimate[] estimates) {
+		// Empty the local model to eliminate duplications
+		EstimateModel.getInstance().emptyModel();
+		
+		// Make sure the response was not null
+		if (estimates != null) {
+			
+			// add the requirements to the local model
+			EstimateModel.getInstance().addEstimates(estimates);
+		}
+	}
+}
--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Estimate.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/Estimate.java
@@ -1,14 +1,169 @@
-/**
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- */
+ * Contributors: The Team8s
+ ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
+
+import java.util.Set;
+import java.util.UUID;
+
+import com.google.gson.Gson;
+
+import edu.wpi.cs.wpisuitetng.janeway.config.ConfigManager;
+import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
 
 /**
  * @author rossfoley
  *
  */
-public class Estimate {
+public class Estimate extends AbstractModel{
+	private int requirementID;
+	private int vote;
+	private String ownerName;
+	private int ownerID;
+	private UUID uuid = UUID.randomUUID();
+	
 	public Estimate() {
 		
 	}
+	
+	//---------------Gets
+	/**
+	 * 
+	 * @return the requirement ID to be estimated
+	 */
+	public int getRequirementID(){
+		return this.requirementID;
+	}
+	
+	/**
+	 * 
+	 * @return returns the ID of the estimate
+	 */
+	public UUID getID(){
+		return this.uuid;
+	}
+	
+	/**
+	 * 
+	 * @return the value to vote for
+	 */
+	public int getVote(){
+		return this.vote;
+	}
+	
+	/**
+	 * 
+	 * @return the ID of the vote creator
+	 */
+	public int getOwnerID(){
+		return this.ownerID;
+	}
+	
+	/**
+	 * 
+	 * @return the name of the creator
+	 */
+	public String getOwnerName(){
+		return this.ownerName;
+	}
+	//---------------Sets
+	/**
+	 * sets the requirementID
+	 * @param requirementID
+	 */
+	public void setRequirementID(int requirementID){
+		this.requirementID = requirementID;
+	}
+	/**
+	 * sets the estimate's ID
+	 * @param ID
+	 */
+	public void setID(UUID ID){
+		this.uuid = ID;
+	}
+	/**
+	 * sets the vote
+	 * @param vote
+	 */
+	public void setVote(int vote){
+		this.vote = vote;
+	}
+	/**
+	 * sets the ID of the owner
+	 * @param ownerID
+	 */
+	public void setOwnerID(int ownerID){
+		this.ownerID = ownerID;
+	}
+	/**
+	 * sets the name of the owner
+	 * @param ownerName
+	 */
+	public void setOwnerName(String ownerName){
+		this.ownerName = ownerName;
+	}
+
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public String toJSON() {
+		return new Gson().toJson(this, Estimate.class);
+	}
+	
+	/**
+	 * 
+	 * @param json json string to convert from
+	 * @return estimate parsed from the json
+	 */
+	public static Estimate fromJson(String json) {
+		final Gson parser = new Gson();
+		return parser.fromJson(json, Estimate.class);
+	}
+
+	/**
+	 * returns an array of estimates from a json string
+	 * @param json string containing a JSON-encoded array of Estimate
+	 * 
+	 * @return an array of Estimates deserialized from the given JSON string
+	 */
+	public static Estimate[] fromJsonArray(String json) {
+		final Gson parser = new Gson();
+		return parser.fromJson(json, Estimate[].class);
+	}
+
+	@Override
+	public Boolean identify(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void copyFrom(Estimate toCopyFrom){
+		this.requirementID = toCopyFrom.getRequirementID();
+		this.vote = toCopyFrom.getVote();
+		this.ownerName = toCopyFrom.getOwnerName();
+		this.ownerID = toCopyFrom.getOwnerID();
+		this.uuid = toCopyFrom.getID();
+	}
+	
+	
 }
