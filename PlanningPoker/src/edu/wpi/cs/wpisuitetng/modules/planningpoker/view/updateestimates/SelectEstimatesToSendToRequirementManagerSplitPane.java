388c5cdb46609958fd79e6b50c6e629732a277a3
Amanda Adkins <aaadkins@wpi.edu>
2014-04-18 16:34:22 -0400

    Create framework panel for showing requirements to update and create a table for selecting which requirements should be estimated

---
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/updateestimates/SelectEstimatesToSendToRequirementManagerSplitPane.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.updateestimates;
+
+import java.awt.Dimension;
+import java.awt.Panel;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.swing.SpringLayout;
+
+import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.PlanningPokerSession;
+
+import javax.swing.JList;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTable;
+
+/**
+ * @author Amanda
+ *
+ */
+public class SelectEstimatesToSendToRequirementManagerSplitPane extends JSplitPane {
+	private PlanningPokerSession currentSession;
+	private List<Integer> reqsWithExportedEstimates;
+	private HashMap<Integer, Integer> finalEstimates;
+	private SpringLayout summaryLayout = new SpringLayout();
+	private JTable selectToUpdateTable;
+	private JList noFinalEstimateList;
+	private JTable alreadySentTable;
+	private JPanel summaryPanel = new JPanel();
+	
+	public SelectEstimatesToSendToRequirementManagerSplitPane(PlanningPokerSession currentSession){
+		this.currentSession = currentSession;
+		reqsWithExportedEstimates = currentSession.getRequirementsWithExportedEstimates();
+		
+		// replace with actual final estimates 
+		finalEstimates = new HashMap<Integer, Integer>();
+		selectToUpdateTable = new SelectRequirementToUpdateTable();
+		buildSummaryPanel();		
+		
+		summaryPanel.setMinimumSize(new Dimension(200, 200));
+		selectToUpdateTable.setMinimumSize(new Dimension(300, 350));
+		
+		setTopComponent(summaryPanel);
+		setBottomComponent(selectToUpdateTable);
+		
+		setDividerLocation(250);	
+	}
+
+	
+	private void buildSummaryPanel(){
+		summaryPanel.setLayout(summaryLayout);
+		
+		summaryLayout.putConstraint(SpringLayout.WEST, noFinalEstimateList, 0, SpringLayout.WEST, summaryPanel);
+		summaryLayout.putConstraint(SpringLayout.EAST, noFinalEstimateList, -40, SpringLayout.HORIZONTAL_CENTER, summaryPanel);
+		
+		summaryLayout.putConstraint(SpringLayout.WEST, alreadySentTable, 10, SpringLayout.EAST, noFinalEstimateList);
+		summaryLayout.putConstraint(SpringLayout.EAST, alreadySentTable, 0, SpringLayout.EAST, summaryPanel);
+		
+		summaryLayout.putConstraint(SpringLayout.NORTH, alreadySentTable, 10, SpringLayout.NORTH, summaryPanel);
+		summaryLayout.putConstraint(SpringLayout.SOUTH, alreadySentTable, -10, SpringLayout.SOUTH, summaryPanel);
+		
+		summaryLayout.putConstraint(SpringLayout.NORTH, noFinalEstimateList, 10, SpringLayout.NORTH, summaryPanel);
+		summaryLayout.putConstraint(SpringLayout.SOUTH, noFinalEstimateList, -10, SpringLayout.SOUTH, summaryPanel);
+		
+		summaryPanel.add(alreadySentTable);
+		summaryPanel.add(noFinalEstimateList);
+	}
+}
