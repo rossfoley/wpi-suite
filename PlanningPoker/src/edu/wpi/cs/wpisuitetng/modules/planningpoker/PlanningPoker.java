4f2836f485b5c6111b7ffc086f6f6a74ba32efd2
Randy Acheson <randy.acheson3@gmail.com>
2014-04-19 02:03:34 -0400

    Makes CodePro fixes on PlanningPoker

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/PlanningPoker.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/PlanningPoker.java
@@ -8,9 +8,6 @@
  *******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.planningpoker;
 
-import java.awt.Color;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,8 +16,6 @@ import javax.swing.*;
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.MainView;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.gui.PlanningPokerSessionTab;
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.PlanningPokerSession;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.ToolbarView;
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.ViewEventController;
 
@@ -33,22 +28,22 @@ import edu.wpi.cs.wpisuitetng.modules.planningpoker.view.ViewEventController;
  */
 public class PlanningPoker implements IJanewayModule {
 	
-	private ArrayList<JanewayTabModel> tabs;
+	private final List<JanewayTabModel> tabs;
 	
 	public PlanningPoker() {
 		tabs = new ArrayList<JanewayTabModel>();
 
-		ToolbarView toolBar = new ToolbarView(true);
-		MainView mainPanel = new MainView();
+		final ToolbarView toolBar = new ToolbarView(true);
+		final MainView mainPanel = new MainView();
 
 		ViewEventController.getInstance().setMainView(mainPanel);
 		ViewEventController.getInstance().setToolBar(toolBar);
 		
 		// Create a tab model that contains the toolbar panel and the main content panel
-		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolBar, mainPanel);
+		final JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolBar, mainPanel);
 
 		// Add the tab to the list of tabs owned by this module
-		tabs.add(tab1);		
+		tabs.add(tab1);
 	}
 	
 	/*
