6951ea994471e7a3f29e38ba1aa198d46238f0db
Rick Wight <fmwight@wpi.edu>
2014-04-20 21:26:15 -0400

    improved Javadoc comments in RequirementEstimateStats

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/statistics/RequirementEstimateStats.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/statistics/RequirementEstimateStats.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 WPI-Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: The Team8s
+ ******************************************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.statistics;
 
 import java.util.Collections;
@@ -5,13 +15,23 @@ import java.util.LinkedList;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Estimate;
 
+/**
+ * This class holds a list of estimates sorted by vote value
+ * and both calculates and holds the mean and median values
+ * for that list of elements 
+ * 
+ * @author Rick Wight (fmwight)
+ */
 public class RequirementEstimateStats {
 	private LinkedList<Estimate> estimates;
 	private double mean;
 	private double median;
 	
-	RequirementEstimateStats() {
-		
+	RequirementEstimateStats(LinkedList<Estimate> givenEstimates) {
+		setEstimates(givenEstimates);
+		sortEstimatesByVote();
+		setMean();
+		setMedian();
 	}
 	
 	/**
