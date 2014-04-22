c3398b12c96aebec4e254d5d2276406c18de8edf
Ross Foley <ross3141@gmail.com>
2014-04-21 21:49:42 -0400

    Only enable the view statistics button on ended and closed sessions

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/buttons/PlanningPokerSessionButtonsPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/buttons/PlanningPokerSessionButtonsPanel.java
@@ -199,10 +199,10 @@ public class PlanningPokerSessionButtonsPanel extends ToolbarGroupView{
 	}
 	
 	public void disableStatisticsButton() {
-		endVoteButton.setEnabled(false);
+		statisticsButton.setEnabled(false);
 	}
 	
 	public void enableStatisticsButton() {
-		endVoteButton.setEnabled(true);
+		statisticsButton.setEnabled(true);
 	}
 }
