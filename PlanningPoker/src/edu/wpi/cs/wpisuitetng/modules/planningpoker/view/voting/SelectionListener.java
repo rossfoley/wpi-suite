6e88b5b386d836053e161b569d01f19308ccd345
William Jones <wcjones@wpi.edu>
2014-04-23 00:08:38 -0400

    Used codePro on SelectionListener.

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/SelectionListener.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/SelectionListener.java
@@ -1,10 +1,7 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.voting;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.EventListener;
 
-import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Estimate;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
 
 public class SelectionListener implements EventListener{
