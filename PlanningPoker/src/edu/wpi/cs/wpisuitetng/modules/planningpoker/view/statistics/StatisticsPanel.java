4b7552b70704903af6450e6d5c58a8898b4523a1
Rick Wight <fmwight@wpi.edu>
2014-04-25 17:32:51 -0400

    statistics button no longer displays for open or pending sessions after a closed or ended session has been selected

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/statistics/StatisticsPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/statistics/StatisticsPanel.java
@@ -71,12 +71,9 @@ public class StatisticsPanel extends JSplitPane implements ISessionTab {
 		reqTable = new StatisticsReqTable(reqData, reqColumnNames);
 		tablePanel = new JScrollPane(reqTable);
 		
-<<<<<<< HEAD
 		// initialize infoPanel
 		infoPanel = detailPanel.getInfoPanel();
 		
-=======
->>>>>>> Rearranges statistics panel layout
 		//set infoPanel to get estimate information for statistics
 		reqTable.setInfoPanel(infoPanel);
 		
@@ -138,10 +135,7 @@ public class StatisticsPanel extends JSplitPane implements ISessionTab {
 		
 		// change the visibility of the top buttons
 		//setButtonVisibility(activeSession);
-<<<<<<< HEAD
 		ViewEventController.getInstance().getPlanningPokerSessionButtonsPanel().disableStatisticsButton();
-=======
->>>>>>> Rearranges statistics panel layout
 	}	
 	
 	//private void updateInfoPanel(PlanningPokerSession session) {
