59f20ef843edb19630ec2c789cf74ed2b6e93d67
Cameron <cdpeterson@wpi.edu>
2014-04-27 20:24:22 -0400

    Added JavaDocs and the beginings of tests

---
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/session/SelectFromListPanelTest.java
@@ -0,0 +1,25 @@
+package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.session;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements.*;
+
+import org.junit.Test;
+
+public class SelectFromListPanelTest {
+
+	@Test
+	public void testFullList() {
+		Requirement rec1 = new Requirement(1, "rec1", "description1"); 
+		Requirement rec2 = new Requirement(2, "rec2", "description2");
+		Requirement rec3 = new Requirement(3, "rec3", "description3");
+		LinkedList<Requirement> testlistofrecs [];
+		
+		
+		
+	}
+
+}
