7b2731f7de13f08a7ba896224ede8b0dff103180
Pat Desmarais <pdesmarais@wpi.edu>
2014-04-28 21:45:47 -0400

    Finished tests for Deck.java in models package

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/DeckTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/models/DeckTest.java
@@ -1,13 +1,15 @@
 package edu.wpi.cs.wpisuitetng.modules.planningpoker.models;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.Test;
+
+import com.google.gson.Gson;
 
 import edu.wpi.cs.wpisuitetng.modules.planningpoker.models.Deck;
 
@@ -123,6 +125,45 @@ public class DeckTest {
 	}
 	
 	@Test public void testtoJSON() {
+		Gson gsontest = new Gson();
+		String jsontest = gsontest.toJson(testDeck);
 		
+		assertEquals(jsontest, testDeck.toJSON());
+	}
+	
+	@Test public void testfromJson() {
+		Gson gsontest = new Gson();
+		String jsontest = gsontest.toJson(testDeck);
+		
+		assertEquals(testDeck, testDeck.fromJson(jsontest));
+	}
+	
+	@Test public void testfromJsonArray() {
+		Gson gsontest = new Gson(); 
+		Deck[] testDeckArray = new Deck[]{testDeck};
+		String jsontest = gsontest.toJson(testDeckArray);
+		
+		assertArrayEquals(testDeckArray, testDeck.fromJsonArray(jsontest));
+		
+	}
+	
+	@Test public void testequals() {
+		int testreq = 1;
+		int testvote = 2;
+		UUID testUUID = null;
+		testUUID.randomUUID();
+		Estimate testest = new Estimate(testreq, testvote, testUUID);
+		List<Integer> testList2 = new ArrayList<Integer>();
+		boolean mult2 = true;
+		Deck testDeck2 = new Deck(testList2, mult2);
+		testDeck.setId(1);
+		testDeck2.setId(2);
+		testDeck.setDeckName("testDeck1");
+		testDeck2.setDeckName("testDeck2");
+		
+		assertFalse(testDeck.equals(null));
+		assertTrue(testDeck.equals(testDeck));
+		assertFalse(testDeck.equals(testest));
+		assertFalse(testDeck.equals(testDeck2));
 	}
 }
