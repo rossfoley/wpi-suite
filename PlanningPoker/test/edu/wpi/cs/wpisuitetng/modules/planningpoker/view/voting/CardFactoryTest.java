ac3581bd0dadeda4216e39660b7163a6c2602a0d
Perry Franklin <pcfranklin@wpi.edu>
2014-05-01 13:58:18 -0400

    Adds test callled GenerationSpeedTest, which is mainly for demostration purposes

--- PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/CardFactoryTest.java
+++ PlanningPoker/test/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/voting/CardFactoryTest.java
@@ -12,6 +12,10 @@ import org.junit.Test;
 
 public class CardFactoryTest {
 	
+	/**
+	 * Tests the generation of a card with 1 digit
+	 * Output goes to tests/TestCard1Digit.png
+	 */
 	@Test
 	public void GenerateCardTest1Digit(){
 		final int number = 4;
@@ -37,6 +41,10 @@ public class CardFactoryTest {
 		}
 	}
 	
+	/**
+	 * Tests the generation of a card with 2 digits
+	 * Output goes to tests/TestCard2Digit.png
+	 */
 	@Test
 	public void GenerateCardTest2Digit(){
 		final int number = 44;
@@ -62,6 +70,10 @@ public class CardFactoryTest {
 		}
 	}
 	
+	/**
+	 * Tests the generation of a card with 3 digits
+	 * Output goes to tests/TestCard3Digit.png
+	 */
 	@Test
 	public void GenerateCardTest3Digit(){
 		final int number = 400;
@@ -85,5 +97,24 @@ public class CardFactoryTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * A demonstration of the time it takes to generate 3 cards in a row.
+	 * Testing on my machine show execution time of this test to be around .3 seconds
+	 */
+	@Test
+	public void GenerationSpeedTest(){
+		CardFactory cF1 = new CardFactory(10);
+		
+		cF1.generateCard();
+		
+		CardFactory cF2 = new CardFactory(10);
+		
+		cF2.generateCard();
+		
+		CardFactory cF3 = new CardFactory(10);
+		
+		cF3.generateCard();
 	}
 }
