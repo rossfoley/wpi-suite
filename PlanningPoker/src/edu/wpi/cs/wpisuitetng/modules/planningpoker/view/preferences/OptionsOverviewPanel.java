ef2bbdec8b39a966c0971c0b3cfdbeb5777de41c
Amanda Adkins <aaadkins@wpi.edu>
2014-05-01 22:34:20 -0400

    Build framework for switching out bottom panels

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/preferences/OptionsOverviewPanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/preferences/OptionsOverviewPanel.java
@@ -13,6 +13,7 @@ package edu.wpi.cs.wpisuitetng.modules.planningpoker.view.preferences;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -32,12 +33,15 @@ public class OptionsOverviewPanel extends JPanel {
 	private JPanel helpPanel = new JPanel();
 	private SpringLayout optionsLayout = new SpringLayout();
 	private SpringLayout comboLayout = new SpringLayout();
+	private SpringLayout lowerLayout = new SpringLayout();
+	private String[] availableOptions = {"Help", "View Decks", "Email Preferences"};
 	
 	public OptionsOverviewPanel(){
 		setLayout(optionsLayout);
+		lowerPanel.setLayout(lowerLayout);
 		
 		buildComboPanel();
-		
+		setBottomPanel(helpPanel);
 		buildOptionsPanel();
 		
 	}
@@ -53,6 +57,8 @@ public class OptionsOverviewPanel extends JPanel {
 		comboLayout.putConstraint(SpringLayout.NORTH, comboLabel, 10, SpringLayout.NORTH, comboPanel);
 		comboLayout.putConstraint(SpringLayout.SOUTH, comboLabel, 10, SpringLayout.SOUTH, comboPanel);
 		
+		comboBoxOptions.setModel(new DefaultComboBoxModel<String>(availableOptions));
+		
 		comboBoxOptions.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				parseComboBoxOptions();
@@ -64,11 +70,29 @@ public class OptionsOverviewPanel extends JPanel {
 	}
 	
 	public void buildOptionsPanel(){
-	
+		optionsLayout.putConstraint(SpringLayout.NORTH, comboPanel, 10, SpringLayout.NORTH, this);
+		optionsLayout.putConstraint(SpringLayout.EAST, comboPanel, -10, SpringLayout.EAST, this);
+		optionsLayout.putConstraint(SpringLayout.WEST, comboPanel, 10, SpringLayout.WEST, this);
 		
+		optionsLayout.putConstraint(SpringLayout.NORTH, lowerPanel, 10, SpringLayout.SOUTH, comboPanel);
+		optionsLayout.putConstraint(SpringLayout.SOUTH, lowerPanel, -10, SpringLayout.SOUTH, this);
+		optionsLayout.putConstraint(SpringLayout.EAST, lowerPanel, -10, SpringLayout.EAST, this);
+		optionsLayout.putConstraint(SpringLayout.WEST, lowerPanel, 10, SpringLayout.WEST, this);
+		
+		add(comboPanel);
+		add(lowerPanel);
 	}
 	
 	public void parseComboBoxOptions(){
 		
+	}
+	
+	public void setBottomPanel(JPanel newBottomPanel){
+		optionsLayout.putConstraint(SpringLayout.NORTH, newBottomPanel, 10, SpringLayout.NORTH, lowerPanel);
+		optionsLayout.putConstraint(SpringLayout.SOUTH, newBottomPanel, -10, SpringLayout.SOUTH, lowerPanel);
+		optionsLayout.putConstraint(SpringLayout.EAST, newBottomPanel, -10, SpringLayout.EAST, lowerPanel);
+		optionsLayout.putConstraint(SpringLayout.WEST, newBottomPanel, 10, SpringLayout.WEST, lowerPanel);
+		
+		lowerPanel.add(newBottomPanel);
 	}
 }
