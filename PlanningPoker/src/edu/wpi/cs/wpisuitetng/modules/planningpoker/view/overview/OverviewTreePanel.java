2336d16fff33ad766bde89c83a82ed2f47332526
Jonathan Dorich <jdorich@wpi.edu>
2014-05-04 22:53:02 -0400

    Removes unused import

--- PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/OverviewTreePanel.java
+++ PlanningPoker/src/edu/wpi/cs/wpisuitetng/modules/planningpoker/view/overview/OverviewTreePanel.java
@@ -20,7 +20,6 @@ import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
